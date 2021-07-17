cf9b67ad62faa1c441322739d98e99ae23ba6df1
smileyblue77 <84291990+smileyblue77@users.noreply.github.com>
2021-07-17 16:30:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var displayName = document.getElementById("secret");
+
+var button = document.getElementById("myButton");
+
+if (button){
+  button.addEventListener("click", displaySecret);  
+}
+
+
+function displaySecret(){
+  var name = document.getElementById("myText").value;
+  var adjective = document.getElementById("adjective").value;
+  var noun = document.getElementById("noun").value;
+
+  displayName.innerHTML = name + ", did you know that when fish sleep, their dreams become " + adjective + " " + noun + "? Shhh, it's a secret!";
+}
